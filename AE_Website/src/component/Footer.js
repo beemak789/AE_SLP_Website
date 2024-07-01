cd4f4780727edc6941814addd8c33c3ac715c7c8
Brandy <81196479+beemak789@users.noreply.github.com>
2024-07-01 19:03:50 -0400

    move footer icons

--- AE_Website/src/component/Footer.js
+++ AE_Website/src/component/Footer.js
@@ -30,28 +30,6 @@ const Footer = () => {
               ></LogoImage>
             </Link>
           </Button>
-
-          <a href='https://www.instagram.com/invites/contact/?igsh=q3arh3rffdow&utm_content=uzf98fk'>
-            {' '}
-            <Icon icon='skill-icons:instagram' />
-          </a>
-
-          <a href='https://www.facebook.com/profile.php?id=61560669335136&mibextid=LQQJ4d'>
-            {' '}
-            <FacebookIcon
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 256 256'
-            >
-              <path
-                fill='#1877f2'
-                d='M256 128C256 57.308 198.692 0 128 0S0 57.308 0 128c0 63.888 46.808 116.843 108 126.445V165H75.5v-37H108V99.8c0-32.08 19.11-49.8 48.348-49.8C170.352 50 185 52.5 185 52.5V84h-16.14C152.959 84 148 93.867 148 103.99V128h35.5l-5.675 37H148v89.445c61.192-9.602 108-62.556 108-126.445'
-              ></path>
-              <path
-                fill='#fff'
-                d='m177.825 165l5.675-37H148v-24.01C148 93.866 152.959 84 168.86 84H185V52.5S170.352 50 156.347 50C127.11 50 108 67.72 108 99.8V128H75.5v37H108v89.445A129 129 0 0 0 128 256a129 129 0 0 0 20-1.555V165z'
-              ></path>
-            </FacebookIcon>
-          </a>
         </div>
 
         <QuickLinksContainer>
@@ -101,6 +79,31 @@ const Footer = () => {
         <ContactInfoContainer>
           <h3>Contact Us</h3>
 
+
+          <IconContainers >
+            <a href='https://www.instagram.com/invites/contact/?igsh=q3arh3rffdow&utm_content=uzf98fk'>
+              {' '}
+              <Icon icon='skill-icons:instagram' />
+            </a>
+
+            <a href='https://www.facebook.com/profile.php?id=61560669335136&mibextid=LQQJ4d'>
+              {' '}
+              <FacebookIcon
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 256 256'
+              >
+                <path
+                  fill='#1877f2'
+                  d='M256 128C256 57.308 198.692 0 128 0S0 57.308 0 128c0 63.888 46.808 116.843 108 126.445V165H75.5v-37H108V99.8c0-32.08 19.11-49.8 48.348-49.8C170.352 50 185 52.5 185 52.5V84h-16.14C152.959 84 148 93.867 148 103.99V128h35.5l-5.675 37H148v89.445c61.192-9.602 108-62.556 108-126.445'
+                ></path>
+                <path
+                  fill='#fff'
+                  d='m177.825 165l5.675-37H148v-24.01C148 93.866 152.959 84 168.86 84H185V52.5S170.352 50 156.347 50C127.11 50 108 67.72 108 99.8V128H75.5v37H108v89.445A129 129 0 0 0 128 256a129 129 0 0 0 20-1.555V165z'
+                ></path>
+              </FacebookIcon>
+            </a>
+          </IconContainers>
+
           <div className='address-container'>
             <img src='/map.png' height={12} width={12} alt='' />
             <div className='address'>
@@ -148,19 +151,10 @@ const FooterContent = styled.div`
 
   .logo-socials {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
     gap: 0.5rem;
-    padding-bottom: 0rem;
 
-    @media (min-width: 375px) and (max-width: 768px) {
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-      gap: 2rem;
-      bottom: 0;
-      margin-left: -12rem;
-    }
   }
 
   @media (min-width: 375px) and (max-width: 768px) {
@@ -201,6 +195,12 @@ const QuickLinksContainer = styled.div`
     display: none;
   }
 `;
+
+const IconContainers = styled.div`
+display: flex;
+gap: 1rem;
+padding: 0.5rem;
+`
 
 const ContactInfoContainer = styled.div`
   display: flex;
